5eecbb90d56b744901f70d990c7e7891dac3c828
fuuta0201 <satofuta0201@gmail.com>
2026-02-20 22:19:18 +0900

    feat: list UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import MainVisual from "./_components/mainVisual";
 import RecentlySection from "./_components/recentlySection";
+import ListSection from "./_components/listSection";
 
 export default function Home() {
   return (
     <main>
       <MainVisual />
       <RecentlySection />
+      <ListSection />
     </main>
   );
 }
--- components/common/sectionTitle.tsx
+++ components/common/sectionTitle.tsx
@@ -1,10 +1,18 @@
+import { cn } from "@/lib/utils";
+
 export default function SectionTitle({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+  className,
+  ...props
+}: React.ComponentProps<"h2">) {
   return (
-    <h2 className="w-full bg-linear-to-l from-indigo-500 via-sky-500 to-emerald-500 bg-clip-text py-4 text-3xl font-extrabold text-transparent">
+    <h2
+      className={cn(
+        "w-full bg-linear-to-l from-indigo-500 via-sky-500 to-emerald-500 bg-clip-text py-4 text-3xl font-extrabold text-transparent",
+        className
+      )}
+      {...props}
+    >
       {children}
     </h2>
   );
