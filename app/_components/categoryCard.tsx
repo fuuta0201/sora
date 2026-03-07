b17087fafd155255790c651dd2af21bc2395a8bd
fuuta0201 <satofuta0201@gmail.com>
2026-03-07 21:36:05 +0900

    feat: category modal UI

--- app/_components/categoryCard.tsx
+++ app/_components/categoryCard.tsx
@@ -7,8 +7,26 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import PostModal from "@/components/common/postModal";
 
 export default function CategoryCard() {
+  const dummy = {
+    id: "ofzwmm11shcj",
+    title: "テスト用記事",
+    body: "テスト用記事です．テスト用記事です．テスト用記事です．",
+    imageUrl: {
+      url: "https://images.microcms-assets.io/assets/e76e0ff2b74748eca57a2ff5147201ad/ffd259d7ddde44bbac0886c4a19e4b0e/4.webp",
+      width: 2000,
+      height: 2667,
+    },
+    category: ["walking"],
+    user: "satofuta0201@gmail.com",
+    createdAt: "2026-02-27T13:37:37.286Z",
+    updatedAt: "2026-02-27T13:45:26.471Z",
+    publishedAt: "2026-02-27T13:37:37.286Z",
+    revisedAt: "2026-02-27T13:45:26.471Z",
+  };
+
   return (
     <Card className="relative mx-auto w-full max-w-sm pt-0">
       <div className="absolute inset-0 z-30 aspect-video rounded-t-xl" />
@@ -26,7 +44,10 @@ export default function CategoryCard() {
         </CardDescription>
       </CardHeader>
       <CardFooter>
-        <Button className="w-full">投稿を見る</Button>
+        <PostModal
+          content={dummy}
+          renderButton={() => <Button className="w-full">投稿を見る</Button>}
+        />
       </CardFooter>
     </Card>
   );
